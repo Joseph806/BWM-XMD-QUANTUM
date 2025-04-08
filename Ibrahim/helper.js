fa24a721962f0aa4013dd5558bec83bdb653b808
Séjœ <jmmwang800@gmail.com>
2025-04-08 14:53:32 +0300

    Update helper.js

--- Ibrahim/helper.js
+++ Ibrahim/helper.js
@@ -1,5 +1,5 @@
 const NEWS_LETTER_JID = "120363285388090068@newsletter"; // Replace with your real one
-const BOT_NAME = "BWM-XMD";
+const BOT_NAME = "JOE-XMD";
 const DEFAULT_THUMBNAIL = "https://files.catbox.moe/sd49da.jpg";
 
 const createContext = (userJid, options = {}) => ({
@@ -21,7 +21,7 @@ const createContext = (userJid, options = {}) => ({
             thumbnailUrl: options.thumbnail || DEFAULT_THUMBNAIL,
             mediaType: 1,
             mediaUrl: options.mediaUrl || undefined,
-            sourceUrl: options.sourceUrl || "https://wa.me/254728782591", // link to bot or business
+            sourceUrl: options.sourceUrl || "https://wa.me/254114305147", // link to bot or business
             showAdAttribution: true,
             renderLargerThumbnail: false 
         }
